440a1d46e554197eba615867cec0b82ef4e98f3a
gretkierewicz <piotr.gretkierewicz@gmail.com>
2020-07-02 21:41:47 +0200

    notes / fix for timestamps (problem at pythonanywhere deployment)

--- notes/models.py
+++ notes/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 from taggit.managers import TaggableManager
 
@@ -11,8 +12,8 @@ from tags.models import TaggedWhatever
 class Note(models.Model):
     title = models.CharField(max_length=128)
     body = models.CharField(max_length=20480, blank=True)
-    created_at = models.DateTimeField('date created', default=datetime.datetime.utcnow)
-    updated_at = models.DateTimeField('date updated', default=datetime.datetime.utcnow)
+    created_at = models.DateTimeField('date created', default=datetime.datetime.now(tz=timezone.utc))
+    updated_at = models.DateTimeField('date updated', default=datetime.datetime.now(tz=timezone.utc))
     creator = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     tags = TaggableManager(through=TaggedWhatever, blank=True)
 
